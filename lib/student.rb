38dea0f4887eed9f583223ce7843edce794ce736
asiamohammed <mohamedasia021@gmail.com>
2023-02-24 08:37:09 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
 
+    require_relative './user'
+
+class Student < User
+    def initialize 
+        @knowledge = []
+    end
+    def learn (know)
+        @knowledge << know
+        
+    end
+    def knowledge
+        @knowledge
+    end
+        
 end
